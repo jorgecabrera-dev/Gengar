773f7fe6d57b3b11e434432019aadbea8674cc1a
jorgecabrera-dev <jorge.cabrera.llanos@gmail.com>
2026-04-20 09:11:05 -0400

    nada nuevo

---
+++ mas ejercicios.py
@@ -0,0 +1,11 @@
+
+entrenador = "Giovanni"
+pkm1 = 6
+
+entrenador2 = "Sabrina"
+pkm2 = 7
+
+if pkm1 <= 6:
+    print("Puede acceder a la liga Pokemon")
+else:
+    print("Tiene más pokemon de los permitidos")    
